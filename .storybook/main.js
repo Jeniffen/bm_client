1a1fc3dc22d28cc03863d1d248866480acf02806
Jeniffen Chandrabalan <c.jeniffen@gmail.com>
2021-04-11 00:24:19 +0200

    switch package management to yarn

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,11 @@
 module.exports = {
-  stories: [
-    "../src/components/.stories/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    "../src/**/**/*.stories.@(js|jsx|ts|tsx)",
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  addons: [
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-  ],
-};
+    "@storybook/preset-create-react-app"
+  ]
+}
