43a0d877ef02b17c58bbda89ae95e7e8f86d5c0a
Jeniffen Chandrabalan <c.jeniffen@gmail.com>
2021-10-05 23:29:22 +0200

    Refactor Button component (#61)
    -close #57

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -11,8 +11,115 @@ export const decorators = [
   ),
 ];
 
+const customViewports = {
+  MotoG4: {
+    name: 'Moto G4',
+    styles: {
+      width: '360px',
+      height: '640px',
+    },
+  },
+  GalaxyS5: {
+    name: 'Galaxy S5',
+    styles: {
+      width: '360px',
+      height: '640px',
+    },
+  },
+  Pixel2: {
+    name: 'Pixel 2',
+    styles: {
+      width: '411px',
+      height: '731px',
+    },
+  },
+  Pixel2XL: {
+    name: 'Pixel 2 XL',
+    styles: {
+      width: '411px',
+      height: '823px',
+    },
+  },
+  iPhone5: {
+    name: 'iPhone 5/SE',
+    styles: {
+      width: '320px',
+      height: '568px',
+    },
+  },
+  iPhone6: {
+    name: 'iPhone 6/7/8',
+    styles: {
+      width: '375px',
+      height: '667px',
+    },
+  },
+  iPhone6Plus: {
+    name: 'iPhone 6/7/8 Plus',
+    styles: {
+      width: '414px',
+      height: '763px',
+    },
+  },
+  iPhoneX: {
+    name: 'iPhone X',
+    styles: {
+      width: '375px',
+      height: '812px',
+    },
+  },
+  iPad: {
+    name: 'iPad',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    },
+  },
+  iPadPro: {
+    name: 'iPad Pro',
+    styles: {
+      width: '1024px',
+      height: '1366px',
+    },
+  },
+  SurfaceDuo: {
+    name: 'Surface Duo',
+    styles: {
+      width: '540px',
+      height: '720px',
+    },
+  },
+  GalaxyFold: {
+    name: 'Galaxy Fold',
+    styles: {
+      width: '280px',
+      height: '653px',
+    },
+  },
+  NestHub: {
+    name: 'Nest Hub',
+    styles: {
+      width: '1024px',
+      height: '600px',
+    },
+  },
+  NestHubMax: {
+    name: 'Nest Hub Max',
+    styles: {
+      width: '1280px',
+      height: '800px',
+    },
+  },
+};
+
 export const parameters = {
-  layout: 'fullscreen',
+  viewport: {
+    defaultViewport: 'iPhoneX',
+    viewports: {
+      ...customViewports,
+    },
+  },
+  layout: ['fullscreen'],
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,95 +1,138 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+
+const defaultBackgroundColor = (buttonType) => {
+  switch (buttonType) {
+    case 'primary':
+      return '#d53f8c';
+    case 'secondary':
+      return '#ffffff';
+    case 'light':
+      return '#ffffff';
+    case 'dark':
+      return '#000000';
+    default:
+      return '#d53f8c';
+  }
+};
+
+const hoverBackgroundColor = (buttonType) => {
+  switch (buttonType) {
+    case 'primary':
+      return '#b83280';
+    case 'secondary':
+      return '#ffffff';
+    case 'light':
+      return '#efefef';
+    case 'dark':
+      return '#000000';
+    default:
+      return '#b83280';
+  }
+};
+
+const defaultColor = (buttonType) => {
+  switch (buttonType) {
+    case 'primary':
+      return '#ffffff';
+    case 'secondary':
+      return '#4a5568';
+    case 'light':
+      return '#484848';
+    case 'dark':
+      return '#a0aec0';
+    default:
+      return '#d53f8c';
+  }
+};
+
+const hoverColor = (buttonType) => {
+  switch (buttonType) {
+    case 'primary':
+      return '#ffffff';
+    case 'secondary':
+      return '#4a5568';
+    case 'light':
+      return '#484848';
+    case 'dark':
+      return '#ffffff';
+    default:
+      return '#d53f8c';
+  }
+};
+
+const defaultMaxtWidth = (size) => {
+  switch (size) {
+    case 'small':
+      return '133px';
+    case 'extra-large':
+      return '528px';
+    default:
+      return '528px';
+  }
+};
+
+const defaultBorder = (buttonType) => {
+  switch (buttonType) {
+    case 'primary':
+      return '2px solid transparent';
+    case 'secondary':
+      return `2px solid #a0aec0`;
+    case 'light':
+      return '2px solid transparent';
+    case 'dark':
+      return '2px solid #a0aec0';
+    default:
+      return '2px solid transparent';
+  }
+};
+
+const hoverBorder = (buttonType) => {
+  switch (buttonType) {
+    case 'primary':
+      return '2px solid transparent';
+    case 'secondary':
+      return `2px solid #000000`;
+    case 'light':
+      return '2px solid transparent';
+    case 'dark':
+      return '2px solid #ffffff';
+    default:
+      return '2px solid transparent';
+  }
+};
 
 export const Button = styled.button`
-
   box-sizing: border-box;
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #fff;
+  border: ${({ buttonType }) => defaultBorder(buttonType)};
   border-radius: 8px;
-  border: none;
   height: 48px;
-  padding-left: 10px;
-  padding-right: 10px;
+  max-width: ${({ size }) => defaultMaxtWidth(size)};
   width: 100%;
+  padding: 1px 10px;
+  background-color: ${({ buttonType }) => defaultBackgroundColor(buttonType)};
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  color: ${({ buttonType }) => defaultColor(buttonType)};
 
   &:hover {
+    border: ${({ buttonType }) => hoverBorder(buttonType)};
+    background-color: ${({ buttonType }) => hoverBackgroundColor(buttonType)};
+    color: ${({ buttonType }) => hoverColor(buttonType)};
     cursor: pointer;
+    transition: background-color 0.2s ease;
   }
 
   &:focus {
     outline: none;
   }
 
-  max-width: ${({ size }) => {
-    switch (size) {
-      case 'small':
-        return '133px';
-      case 'extra-large':
-        return '528px';
-      default:
-        return '528px';
-    }
-  }};
-
-  ${({ btnType }) =>
-    btnType === 'primary' &&
-    css`
-      background-color: #d53f8c;
-      color: #fff;
-
-      &:hover {
-        transition: background-color 0.2s ease;
-        background-color: #b83280;
-      }
-    `}
-
-  ${({ btnType }) =>
-    btnType === 'secondary' &&
-    css`
-      border: 2px solid #a0aec0;
-      background-color: #fff;
-      color: #4a5568;
-
-      &:hover {
-        border: 2px solid #000;
-      }
-    `}
-
   & img {
     position: absolute;
     left: 0;
   }
-
-  ${({ btnType }) =>
-    btnType === 'dark' &&
-    css`
-      border: 2px solid #a0aec0;
-      background-color: #000;
-      color: #a0aec0;
-
-      &:hover {
-        border: 2px solid #fff;
-        color: #fff;
-      }
-    `}
-
-  }
-
-  ${({ btnType }) =>
-    btnType === 'light' &&
-    css`
-      background-color: #fff;
-      color: #484848;
-
-      &:hover {
-        background-color: #efefef;
-      }
-    `}
-
-  }
 `;
 
-export default { Button };
+const Style = { Button };
+export default Style;
