8e675d50bcd6ca19e36695e3e5ec652e50467df5
Jeniffen Chandrabalan <c.jeniffen@gmail.com>
2021-11-21 16:45:12 -0500

    Clean-up project (#73)

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Header } from './views/Header';
 import { Home } from './views/Home';
-import { Footer } from './components/Footer';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 export const Routes = () => {
   return (
-    <div>
+    <>
       <Switch>
         <Route exact path="/Home" component={Home} />
         <Route exact path="/">
           <Redirect to="/Home" />
         </Route>
       </Switch>
-      <Footer />
-    </div>
+    </>
   );
 };
