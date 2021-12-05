d6f4d6419473b80915dedb30b6715a4e2b322c32
Jeniffen Chandrabalan <c.jeniffen@gmail.com>
2021-12-05 21:57:16 +0100

    Minor refactor

---
+++ src/components/Sections/Explore/Explore.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Style from './styles';
+
+const Explore = () => {
+  return <Style.ExploreWrapper />;
+};
+
+Explore.propTypes = {};
+
+export default Explore;
---
+++ src/components/Sections/Explore/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const ExploreWrapper = styled.div`
+  /* Default styles */
+  /* box model */
+  box-sizing: border-box;
+  height: calc(1.3 * 100vw);
+  width: 100%;
+`;
+
+const Style = { ExploreWrapper };
+export default Style;
